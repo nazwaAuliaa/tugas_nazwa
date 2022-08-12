64043625e513a2b278bcc2cd85a3275cd165791c
nazwaAuliaa <110008117+nazwaAuliaa@users.noreply.github.com>
2022-08-12 09:22:47 +0700

    Add files via upload

--- proses_edit.php
+++ proses_edit.php
@@ -5,7 +5,7 @@
  $id = $_POST['id'];
 
  $database = new PDO("mysql:host=localhost;dbname=ppdb",'root','');
- $query = $database->query("UPDATE `siswa` SET nama='$nama',asal_sekolah='$asal' WHERE id='$id'");
+ $query = $database->query("UPDATE `calon_siswa ` SET nama='$nama',asal_sekolah='$asal' WHERE id='$id'");
 
  if($query){
     header("Location:index.php");
